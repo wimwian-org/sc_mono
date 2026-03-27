2be136cf179aafb094ba6107c44e3378025a2233
ANAND PANCHAPAKESAN <anand.panchapakesan@ramco.com>
2026-03-27 11:49:18 +0530

    chore: convert Button to submodule

--- src/components/Button/Button.svelte.d.ts
+++
@@ -1,4 +0,0 @@
-import type { ButtonProps } from './ButtonTypes.js';
-declare const Button: import("svelte").Component<ButtonProps, {}, "">;
-type Button = ReturnType<typeof Button>;
-export default Button;
--- src/components/Button/ButtonLink.svelte.d.ts
+++
@@ -1,4 +0,0 @@
-import type { ButtonLinkProps } from './ButtonLinkTypes.js';
-declare const ButtonLink: import("svelte").Component<ButtonLinkProps, {}, "">;
-type ButtonLink = ReturnType<typeof ButtonLink>;
-export default ButtonLink;
